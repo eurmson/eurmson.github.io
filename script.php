67fd060a80d4049d0bfa6c3123584b9a9545f239
Ethan Urmson <56134017+eurmson@users.noreply.github.com>
2023-03-17 20:03:19 -0400

    Update script.php

--- script.php
+++ script.php
@@ -1,26 +1,26 @@
 <?php
 
-// function full_copy( $source, $target ) {
-//     if ( is_dir( $source ) ) {
-//         @mkdir( $target );
-//         $d = dir( $source );
-//         while ( FALSE !== ( $entry = $d->read() ) ) {
-//             if ( $entry == '.' || $entry == '..' ) {
-//                 continue;
-//             }
-//             $Entry = $source . '/' . $entry; 
-//             if ( is_dir( $Entry ) ) {
-//                 full_copy( $Entry, $target . '/' . $entry );
-//                 continue;
-//             }
-//             copy( $Entry, $target . '/' . $entry );
-//         }
+function full_copy( $source, $target ) {
+    if ( is_dir( $source ) ) {
+        @mkdir( $target );
+        $d = dir( $source );
+        while ( FALSE !== ( $entry = $d->read() ) ) {
+            if ( $entry == '.' || $entry == '..' ) {
+                continue;
+            }
+            $Entry = $source . '/' . $entry; 
+            if ( is_dir( $Entry ) ) {
+                full_copy( $Entry, $target . '/' . $entry );
+                continue;
+            }
+            copy( $Entry, $target . '/' . $entry );
+        }
 
-//         $d->close();
-//     }else {
-//         copy( $source, $target );
-//     }
-// }
+        $d->close();
+    }else {
+        copy( $source, $target );
+    }
+}
 
 mkdir("build")
 echo getcwd();
